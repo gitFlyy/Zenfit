490f6cd059c1514f43cc9f9e6f93b0dfaa12fb86
Hanzlah Ch <hanzlahch1234@gmail.com>
2025-10-31 02:04:53 +0500

    Settings Screen Implemented

---
+++ app/src/main/java/com/example/zenfit/Settings.kt
@@ -0,0 +1,34 @@
+package com.example.zenfit
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val settingsBtn = findViewById<RelativeLayout>(R.id.account)
+        settingsBtn.setOnClickListener {
+            val intent = Intent(this, Account::class.java)
+            startActivity(intent)
+        }
+        val changePasswordBtn = findViewById<RelativeLayout>(R.id.changePasswordOption)
+        changePasswordBtn.setOnClickListener {
+            val intent = Intent(this, ChangePassword::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
