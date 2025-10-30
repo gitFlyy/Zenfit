6670a38eedfa6bcf39b172b6d4ecf01ad25e8ef5
Hanzlah Ch <hanzlahch1234@gmail.com>
2025-10-31 02:02:01 +0500

    SignUp Screen Implemented

---
+++ app/src/main/java/com/example/zenfit/SignUp.kt
@@ -0,0 +1,27 @@
+package com.example.zenfit
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val continueButton = findViewById<Button>(R.id.continueButton)
+        continueButton.setOnClickListener {
+            val intent = Intent(this, Signup2::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/zenfit/Signup2.kt
@@ -0,0 +1,46 @@
+package com.example.zenfit
+
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
+
+class Signup2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val dateInput = findViewById<EditText>(R.id.dateOfBirthInput)
+
+        dateInput.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePicker = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                dateInput.setText(selectedDate)
+            }, year, month, day)
+
+            datePicker.show()
+        }
+
+        val continueButton = findViewById<Button>(R.id.createAccountButton)
+        continueButton.setOnClickListener {
+            val intent = Intent(this, SetupScreen::class.java)
+            startActivity(intent)
+        }
+    }
+}
