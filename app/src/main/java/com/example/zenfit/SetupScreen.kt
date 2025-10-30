cc701188c47ef908611b930c865059ea5f57796a
Hanzlah Ch <hanzlahch1234@gmail.com>
2025-10-31 02:05:47 +0500

    Profile Screen Implemented

---
+++ app/src/main/java/com/example/zenfit/SetupScreen.kt
@@ -0,0 +1,29 @@
+package com.example.zenfit
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SetupScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_setup_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val finishBtn = findViewById<Button>(R.id.finishBtn)
+        finishBtn.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
